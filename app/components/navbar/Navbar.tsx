26f865ac4f01d58ac02f9bfc2512c6d8fc9c235f
iinsue <hinsu93@gmail.com>
2023-06-27 22:27:11 +0900

    client side 설정

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,12 @@
+import Container from "../Container";
+
 const Navbar = () => {
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">I am a Navbar</div>
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>anything</Container>
+      </div>
+    </div>
   );
 };
 
