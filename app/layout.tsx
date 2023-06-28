3daa0ac2a53169fec1e2c3ce22e0193e76570000
iinsue <hinsu93@gmail.com>
2023-06-28 22:39:28 +0900

    Button 설정 마무리

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Hello World" isOpen />
+          <Modal title="Hello World" isOpen actionLabel="Submit" />
           <Navbar />
         </ClientOnly>
         {children}
