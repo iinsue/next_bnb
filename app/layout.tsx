16d94f38a3689708bccec081fb8a6073162a96aa
iinsue <hinsu93@gmail.com>
2023-06-28 23:09:27 +0900

    등록 모달 설정 마무리
    axios 와 react-hook-form 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 // Tab Name
 export const metadata = {
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Hello World" isOpen actionLabel="Submit" />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
