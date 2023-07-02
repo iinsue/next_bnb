d9047889825813f6be00c1b46fe2afa9f565e9b9
iinsue <hinsu93@gmail.com>
2023-07-02 15:10:26 +0900

    CSR Warning 제거

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -4,16 +4,16 @@ import { useCallback, useMemo } from "react";
 import { useRouter } from "next/navigation";
 import { format } from "date-fns";
 
-import { Listing, Reservation } from "@prisma/client";
+import { Reservation } from "@prisma/client";
 
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import useCountries from "@/app/hooks/useCountries";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
