25dd7dae1c163142d6eca26c9739d5c6d273dcab
iinsue <hinsu93@gmail.com>
2023-07-02 16:04:12 +0900

    상세 페이지 상단 구현

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,6 +1,9 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingsById from "@/app/actions/getListingById";
+
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
+import ListingClient from "./ListingClient";
 
 interface IParams {
   listingId?: string;
@@ -8,6 +11,8 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingsById(params);
+  const currentUser = await getCurrentUser();
+
   if (!listing) {
     return (
       <ClientOnly>
@@ -15,7 +20,11 @@ const ListingPage = async ({ params }: { params: IParams }) => {
       </ClientOnly>
     );
   }
-  return <ClientOnly>{listing.title}</ClientOnly>;
+  return (
+    <ClientOnly>
+      <ListingClient listing={listing} currentUser={currentUser} />
+    </ClientOnly>
+  );
 };
 
 export default ListingPage;
