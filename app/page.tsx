f4c0e664587476b72957bae6e2aa5ec999600c63
iinsue <hinsu93@gmail.com>
2023-07-02 12:57:10 +0900

    사진 리스트로 만들기

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListing";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -31,7 +34,13 @@ export default async function Home() {
         "
         >
           {listings.map((listing: any) => {
-            return <div>{listing.title}</div>;
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
           })}
         </div>
       </Container>
